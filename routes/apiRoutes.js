cde4b2d767a3f1cd849f53cd6ab8433f9a50233a
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-20 17:38:51 -0700

    Clearing out console logs in db.json and apiRoutes.js.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,24 @@
-// ===============================================================================
+
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
 // ===============================================================================
 const create = require("../db/create");
 const notesData = require("../db/db.json");
-const fs = require("fs");
-// const { create  = require("");
-const db = [];
-// var waitListData = require("../data/waitinglistData");
 
-// ===============================================================================
 // ROUTING
 // ===============================================================================
 module.exports = function (app) {
+
     // API GET Requests
     // ---------------------------------------------------------------------------
 
     // When the user inputs /api/notes, they will be returned the db.json information.
     app.get("/api/notes", function (req, res) {
-        // console.log("I am app.get");
+        
         create
             .getNotes()
             .then(function (notes) {
-                // console.log(notes);
                 return res.json(notes)
             })
-
-        // console.log("Notes data: " + notesData[0].id);
-        // res.json(notesData);
     });
 
 
@@ -43,23 +34,16 @@ module.exports = function (app) {
             .addNotes(req.body.title, req.body.text)
             .then((notes) => res.json(notes))
             .catch((err) => console.log(err));
-        console.log("added notes, req.body.title: " + req.body.title);
+       
     });
-
 
     // ---------------------------------------------------------------------------
     // Displays a single note
     app.get("/api/notes/:id", function (req, res) {
 
-        // var notesDataString = JSON.stringify(notesData);
-        // console.log("Notes Data:" + notesDataString);
-
-        // console.log("Hello Query", query)
         // Grab the selected parameter
         var chosen = req.params.id;
-        console.log("chosen.id: " + chosen);
-        // console.log(chosen === "3");
-
+      
         // Filter to show only the selected character
         for (var i = 0; i < notesData.length; i++) {
             console.log("notesData: " + JSON.stringify(notesData[i].id));
@@ -71,6 +55,7 @@ module.exports = function (app) {
             }
         }
     });
+
     // DELETE note operation
     app.delete("/api/notes/:id", function (req, res) {
         console.log("delete was called");
